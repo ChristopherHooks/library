e3a370700411967e8483e02b83c5bb2ee571ba5e
Christopher Hooks <christopher.hooks89@gmail.com>
2025-01-25 22:28:54 -0600

    Add basic html elements, styles, add hard coded books for testing.

--- script.js
+++ script.js
@@ -13,5 +13,28 @@ function Book(title, author, pages, read) {
 }
 
 const theHobbit = new Book("The Hobbit", "J.R.R Tolkien", 295, false);
+const lostGods = new Book('Lost Gods', 'Brom', 300, true);
+const theChildThief = new Book('The Child Thief', 'Brom', 600, true ); 
+const slewfoot = new Book('Slewfoot', 'Brom', 545, true);
+
+
 
 console.log(theHobbit.info()); // "The Hobbit by J.R.R. Tolkien, 295 pages, not read yet
+
+
+function addBookToLibrary(book) {
+    myLibrary.push(book);
+}
+
+function showLibrary(){
+    for(const book in library){
+        
+    }
+}
+
+const myLibrary = [];
+
+
+
+
+
